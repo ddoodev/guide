0a8d4acb720fab198c7adff787df845c49ca0a93
Mirdukkk <vdvmirduk@gmail.com>
2021-07-19 02:46:10 +0300

    fix: fix pages switch animation on firefox/edge/chrome

--- docs/javascripts/pagetransition.js
+++ docs/javascripts/pagetransition.js
@@ -35,7 +35,7 @@ if (window.jQuery && window.innerWidth > 1220) {
       loadingInner: '', // e.g '<img src="loading.svg" />'
       timeout: false,
       timeoutCountdown: 5000,
-      onLoadEvent: true,
+      onLoadEvent: false,
       browser: [ 'animation-duration', '-webkit-animation-duration'],
       // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
       // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
@@ -44,6 +44,60 @@ if (window.jQuery && window.innerWidth > 1220) {
       overlayParentElement : 'body',
       transition: function(url){ window.location.href = url; }
     })
+
+    $('main').animsition('in')
   })
+
+  let baseUrl = ''
+
+  function observe() {
+    const targetNode = document.querySelector('[data-md-component="main"]');
+
+    const observer = new MutationObserver(mutations => {
+      console.log('муташн))')
+      mutations.forEach(mutation => {
+        console.log(')))', mutation)
+        if (mutation.type === 'attributes') {
+          console.log('))))')
+          console.log(mutation.target.baseURI, baseUrl)
+          if (mutation.target.baseURI.replace(/#(.)+/g, '') !== baseUrl.replace(/#(.)+/g, '')) {
+            console.log('))))))))))))')
+            baseUrl = mutation.target.baseURI
+            $(".animsition").animsition({
+              inClass: 'fade-in',
+              outClass: 'fade-out',
+              inDuration: 200,
+              outDuration: 200,
+              linkElement: 'a:not([target="_blank"]):not([href*="#"])',
+              // e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
+              loading: false,
+              loadingParentElement: 'main', //animsition wrapper element
+              loadingClass: 'animsition-loading',
+              loadingInner: '', // e.g '<img src="loading.svg" />'
+              timeout: false,
+              timeoutCountdown: 5000,
+              onLoadEvent: false,
+              browser: [ 'animation-duration', '-webkit-animation-duration'],
+              // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
+              // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
+              overlay : false,
+              overlayClass : 'animsition-overlay-slide',
+              overlayParentElement : 'body',
+              transition: function(url){ window.location.href = url; }
+            })
+
+            $('main').animsition('in')
+            observer.disconnect()
+            observe()
+          }
+        }
+      })
+    });
+
+    observer.observe(targetNode, { attributes: true, childList: true, subtree: true });
+  }
+
+  observe()
+
 }
 
