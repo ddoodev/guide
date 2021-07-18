05c8b84c7489ab372b550b2f1788c7004920593b
Mirdukkk <vdvmirduk@gmail.com>
2021-07-18 22:57:26 +0300

    fix: try to import pages switch animation library, but not use it

--- docs/javascripts/pagetransition.js
+++ docs/javascripts/pagetransition.js
@@ -11,39 +11,41 @@ $('body').mPageTransition({
 
 });*/
 
-if (false) {
+!async function () {
+  if (window.jQuery && window.innerWidth > 1220) {
+    /*
+    document$.subscribe(() => {
+      const elements = document.getElementsByTagName('main');
 
-  document$.subscribe(() => {
-    const elements = document.getElementsByTagName('main');
+      for (let element of elements) {
+        if (!element.classList.contains('animsition')) element.classList.add('animsition')
+      }
+    })
 
-    for (let element of elements) {
-      if (!element.classList.contains('animsition')) element.classList.add('animsition')
-    }
-  })
+    $(document).ready(function() {
+      $(".animsition").animsition({
+        inClass: 'fade-in',
+        outClass: 'fade-out',
+        inDuration: 200,
+        outDuration: 200,
+        linkElement: 'a:not([target="_blank"]):not([href*="#"])',
+        // e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
+        loading: false,
+        loadingParentElement: 'main', //animsition wrapper element
+        loadingClass: 'animsition-loading',
+        loadingInner: '', // e.g '<img src="loading.svg" />'
+        timeout: false,
+        timeoutCountdown: 5000,
+        onLoadEvent: true,
+        browser: [ 'animation-duration', '-webkit-animation-duration'],
+        // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
+        // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
+        overlay : false,
+        overlayClass : 'animsition-overlay-slide',
+        overlayParentElement : 'body',
+        transition: function(url){ window.location.href = url; }
+      })
+    })*/
+  }
+}()
 
-  $(document).ready(function() {
-    $(".animsition").animsition({
-      inClass: 'fade-in',
-      outClass: 'fade-out',
-      inDuration: 200,
-      outDuration: 200,
-      linkElement: 'a:not([target="_blank"]):not([href*="#"])',
-      // e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
-      loading: false,
-      loadingParentElement: 'main', //animsition wrapper element
-      loadingClass: 'animsition-loading',
-      loadingInner: '', // e.g '<img src="loading.svg" />'
-      timeout: false,
-      timeoutCountdown: 5000,
-      onLoadEvent: true,
-      browser: [ 'animation-duration', '-webkit-animation-duration'],
-      // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
-      // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
-      overlay : false,
-      overlayClass : 'animsition-overlay-slide',
-      overlayParentElement : 'body',
-      transition: function(url){ window.location.href = url; }
-    });
-  });
-
-}
