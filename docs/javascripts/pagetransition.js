709def89156961114fe997751a1000a3ebddfec2
Mirdukkk <vdvmirduk@gmail.com>
2021-07-19 02:53:30 +0300

    chore: remove unnecessary code

--- docs/javascripts/pagetransition.js
+++ docs/javascripts/pagetransition.js
@@ -21,33 +21,6 @@ if (window.jQuery && window.innerWidth > 1220) {
     }
   })
 
-  $(document).ready(function() {
-    $(".animsition").animsition({
-      inClass: 'fade-in',
-      outClass: 'fade-out',
-      inDuration: 200,
-      outDuration: 200,
-      linkElement: 'a:not([target="_blank"]):not([href*="#"])',
-      // e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
-      loading: false,
-      loadingParentElement: 'main', //animsition wrapper element
-      loadingClass: 'animsition-loading',
-      loadingInner: '', // e.g '<img src="loading.svg" />'
-      timeout: false,
-      timeoutCountdown: 5000,
-      onLoadEvent: false,
-      browser: [ 'animation-duration', '-webkit-animation-duration'],
-      // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
-      // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
-      overlay : false,
-      overlayClass : 'animsition-overlay-slide',
-      overlayParentElement : 'body',
-      transition: function(url){ window.location.href = url; }
-    })
-
-    $('main').animsition('in')
-  })
-
   let baseUrl = ''
 
   function observe() {
